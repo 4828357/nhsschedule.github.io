212eee37a0ca7c69df74212df826f194c075a5ff
nhsschedule <56374746+nhsschedule@users.noreply.github.com>
2020-08-12 17:42:17 -0400

    Add files via upload

---
+++ speakscript.js
@@ -0,0 +1,11 @@
+function letsSpeak() {
+  let sentenceTest = document.getElementById('theText');
+  console.log(sentenceTest.value);
+  var u1 = new SpeechSynthesisUtterance(sentenceTest.value);
+  u1.lang = 'en-US';
+  u1.pitch = 1;
+  u1.rate = 1.5;
+  u1.voiceURI = 'native';
+  u1.volume = 1;
+  speechSynthesis.speak(u1);
+}
