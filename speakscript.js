7d40a904ec85938e93dad1919539d14b1424d779
nhsschedule <56374746+nhsschedule@users.noreply.github.com>
2021-03-12 09:55:40 -0500

    Update speakscript.js

--- speakscript.js
+++ speakscript.js
@@ -4,6 +4,19 @@ function letsSpeak() {
   var u1 = new SpeechSynthesisUtterance(sentenceTest.value);
   u1.lang = 'en-US';
   u1.pitch = 1;
+  u1.rate = 1.5;
+  u1.voiceURI = 'native';
+  u1.volume = 1;
+  speechSynthesis.speak(u1);
+}
+
+function letsSpeakSecond(read) {
+  let sentenceTest = read;
+  console.log(sentenceTest);
+  console.log(sentenceTest.value);
+  var u1 = new SpeechSynthesisUtterance(sentenceTest);
+  u1.lang = 'en-US';
+  u1.pitch = 1;
   u1.rate = 0.01;
   u1.voiceURI = 'native';
   u1.volume = 1;
